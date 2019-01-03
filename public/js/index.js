5a914471c869cc185f06f6ee331467bb87384471
Nevaan Perera <nevaan9@gmail.com>
2019-01-03 00:02:58 -0500

    Added CSS

--- public/js/index.js
+++ public/js/index.js
@@ -36,10 +36,9 @@ jQuery('#message-form').on('submit', function (e) {
     socket.emit('createMessage', {
         from: 'User',
         msg: jQuery('[name=message]').val()
-    }, function (data) {
-        console.log(`This was sent from the server: ${data}`);
+    }, function () {
+        jQuery('#message-value').val('');
     });
-    jQuery('#message-value').val('');
 });
 
 // Send location button
@@ -50,13 +49,17 @@ sendLocButton.on('click', function () {
         return alert('Geolocation not supported by browser.');
     }
 
+    sendLocButton.attr('disabled', 'disabled').text('Getting location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        sendLocButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             from: 'User',
             lat: position.coords.latitude,
             lng: position.coords.longitude,
         })
     }, function () {
+        sendLocButton.removeAttr('disabled').text('Send location');
         alert('Unable to share location');
     })
 });
